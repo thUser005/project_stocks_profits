5cfdecdf6696d48e540b2281c827596bb2ef975e
7b809 <ej818793@gmail.com>
2026-01-01 09:33:51 +0530

    none

--- telegram_msg.py
+++ telegram_msg.py
@@ -1,8 +1,10 @@
 import os
 import requests
 
+BOT = os.environ["TELEGRAM_BOT_TOKEN"]
+CHAT = os.environ["TELEGRAM_CHAT_ID"]
 
-def send_message(text,BOT,CHAT):
+def send_message(text):
     requests.post(
         f"https://api.telegram.org/bot{BOT}/sendMessage",
         data={"chat_id": CHAT, "text": text},
